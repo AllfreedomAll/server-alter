e6bac65e4154b56def8cf8ab221d007206382d79
zhangyue <zhangyue9911@gmail.com>
2025-08-14 17:32:48 +0800

    init3

---
+++ src/settings/prod.py
@@ -0,0 +1,8 @@
+from .base import *
+
+# CREATE DATABASE BiManagerzok DEFAULT CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+REDIS_SERVER_IP = "localhost"
+REDIS_SERVER_PORT = 6379
+REDIS_DB_NO = 5
+DEBUG = False
+
